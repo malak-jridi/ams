76b45224b5975891872d29b41ced0525a2084530
malak-jridi <malakjridi23@gmail.com>
2022-02-06 15:34:42 +0100

    FEATURE exercice th
    REF #[]()
    
    Details
    ------------------
    
    Others
    ------------------

--- src/main/java/com/sip/ams/controllers/HomeController.java
+++ src/main/java/com/sip/ams/controllers/HomeController.java
@@ -1,13 +1,28 @@
 package com.sip.ams.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList; // import the ArrayList class
+
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/info")
-	public String info() {
+	public String info(Model model) {
+		String formation = "FullStack";
+		ArrayList<String> values = new ArrayList<String>(); // Create an ArrayList object
+		values.add("OCP");
+		values.add("OCA");
+		values.add("Spring");
+		values.add("Angular");
+
+		System.out.println("méthode info");
+		model.addAttribute("workshop", formation);
+		model.addAttribute("vals", values);
+
+		
 		return "home/info";
 	}
 	
